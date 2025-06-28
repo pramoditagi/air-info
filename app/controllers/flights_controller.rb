a9fb1c5017934f2fa13118f680fb9c123354bd91
Satyapramod Itagi <itagipramod@gmail.com>
2025-06-28 23:16:49 +0530

    Remove unwanted logic of showing toggle for flights (#4)
    * Remove unwanted logic of showing toggle for flights
    
    * Updated statuses in logical order

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -65,31 +65,6 @@ class FlightsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def flight_params
-      permitted_params = params.require(:flight).permit(:std_time, :std_datetime, :etd_time, :etd_datetime, :airline, :flight_no, :destination, :gate, :status)
-      
-      # Convert time fields to datetime if they exist
-      if permitted_params[:std_time].present?
-        time = Time.parse(permitted_params[:std_time])
-        permitted_params[:std] = Time.current.change(hour: time.hour, min: time.min)
-        permitted_params.delete(:std_time)
-        permitted_params.delete(:std_datetime)
-      elsif permitted_params[:std_datetime].present?
-        permitted_params[:std] = permitted_params[:std_datetime]
-        permitted_params.delete(:std_time)
-        permitted_params.delete(:std_datetime)
-      end
-      
-      if permitted_params[:etd_time].present?
-        time = Time.parse(permitted_params[:etd_time])
-        permitted_params[:etd] = Time.current.change(hour: time.hour, min: time.min)
-        permitted_params.delete(:etd_time)
-        permitted_params.delete(:etd_datetime)
-      elsif permitted_params[:etd_datetime].present?
-        permitted_params[:etd] = permitted_params[:etd_datetime]
-        permitted_params.delete(:etd_time)
-        permitted_params.delete(:etd_datetime)
-      end
-      
-      permitted_params
+      params.require(:flight).permit(:std, :etd, :airline, :flight_no, :destination, :gate, :status)
     end
 end
